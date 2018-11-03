507831ec3b6f3b64fb8e3a763b05e13c9476e606
Aayush <aayushpokharel36@gmail.com>
2018-11-02 21:30:10 -0400

    some minor tweaks and grammar corrections

--- pattern.js
+++ pattern.js
@@ -1,6 +1,6 @@
 
 
-number = prompt("type a number of last star to print '5-50' ")
+number = prompt("type a number and see what happens my suggestion '5-50' & type 10000 if you are viewing my site in Super Computer ")
 
 
 for(i=1; i <= number; i++){
