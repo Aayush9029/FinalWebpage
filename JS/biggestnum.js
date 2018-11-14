44f523b5244d4f9dfa3dfccbc21a2378c01be929
Aayush <aayushpokharel36@gmail.com>
2018-11-14 18:05:10 -0500

    clean up and unity

---
+++ JS/biggestnum.js
@@ -0,0 +1,7 @@
+
+
+que1 = prompt("Type first number");
+
+que2 = prompt("Type second number");
+
+que3 = prompt("Type third number");
--- JS/introJQ.js
+++ JS/introJQ.js
@@ -1,8 +1,8 @@
 
-$(function() {
-    $("#ok").hide(1000).delay(500).show(2000)
+// $(function() {
+//     $("#ok").hide(1000).delay(500).show(2000)
 
 
-        $('ul').hide(1000).show(2000)
-});
+//         $('ul').hide(1000).show(2000)
+// });
 
