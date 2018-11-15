a3c946ba3bb22e658b4e34f424b32fe9bb55ff98
Aayush <aayushpokharel36@gmail.com>
2018-11-14 19:26:24 -0500

    index page questions

--- JS/biggestnum.js
+++ JS/biggestnum.js
@@ -1,7 +1,22 @@
 
+var num1 = Number(prompt("number 1"));
 
-que1 = prompt("Type first number");
+var num2 = Number(prompt("num 2 "));
 
-que2 = prompt("Type second number");
+var num3 = Number(prompt("num3?"));
 
-que3 = prompt("Type third number");
+
+const courses = [num1, num2, num3];
+
+console.log("Original values "+ courses);
+
+sum = (num1 + num2 + num3);
+
+console.log("Average of the numbers = " + sum/courses.length);
+
+console.log("In asscending orders ");
+
+console.log(courses[0] +" "+  courses[1] +" "+ courses[2]);
+
+alert("Console log for original values, their average and to view them in asscending orders ")
+
