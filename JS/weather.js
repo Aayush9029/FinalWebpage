33e64787de921005aa4bc87df7e442790da69770
Aayush <aayushpokharel36@gmail.com>
2018-11-30 16:36:14 -0500

    added a weather module
    yayyyy finnaly done it's fun more if and else and calculations need to be added including new gui :+1:

---
+++ JS/weather.js
@@ -0,0 +1,51 @@
+$(document).ready(function () {
+    
+  // Get Location 
+  navigator.geolocation.getCurrentPosition(success, error);
+
+  function success(pos) {
+      var lat = pos.coords.latitude;
+      var long = pos.coords.longitude;
+      weather(lat, long);
+  }
+
+  function error() {
+      console.log('There was an error');
+  }
+
+  // Call Weather
+  function weather(lat, long) {
+      var URL = `https://api.openweathermap.org/data/2.5/weather?APPID=bc491408821cc43317006173fd1c5bef&lat=${lat}&lon=${long}&units=metric`;
+
+      $.getJSON(URL, function(data) {
+          updateDOM(data);
+          console.log(data);
+      });
+  }
+
+  // Update Dom
+  function updateDOM(data) {
+      var city = (data.name);
+      var temp = Math.round(data.main.temp);
+      var desc = data.weather[0].description;
+      var greeting;
+      
+        if (temp > 10) {
+        greeting = "no jacket necessary";
+
+         }else if (temp < 5){
+          greeting ='Heavy Jacket';
+          }else {
+         greeting ='niceday';
+        }
+
+      $('#city').html(city.toUpperCase());
+      $('#temp').html(temp);
+      $('#desc').html(desc.toUpperCase());
+      $('#wear').html(greeting);
+
+  }
+});
+
+
+document.write(greeting);
