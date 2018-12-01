764dd20747ef851c5dabdb4bb2fe3dca26a153f1
Aayush <aayushpokharel36@gmail.com>
2018-12-01 12:29:18 -0500

    btoa dec and enc

--- JS/encrypt.js
+++ JS/encrypt.js
@@ -8,10 +8,28 @@
 
 
 function encrypt(){  
-    var chars = {'a':'b','b':'c','c':'d','d':'e', 'e':'f', 'f':'g', 'g':'h', 'h':'i', 'i':'j', 'j':'k', 'k':'l', 'l':'m', 'm':'n', 'n':'o', 'o':'p', 'p':'q','q':'r','r':'s','s':'t','t':'u', 'u':'v', 'v':'w', 'w':'x', 'x':'y', 'y':'z', 'z':'a', '0':'1', '1':'2', '2':'3', '3':'4'};
-    var enc;
+   
     var str = document.getElementById("raw").value;
 
-            enc = str.replace(/[abcdefghijklmnopqrstuvwxyz123]/g, m => chars[m]);
+    
+    var enc = btoa(str);
+
+    var enc2= enc.toUpperCase(2);
+
+    console.log(enc2);
+         
             document.getElementById("raw").value = enc;
+}   
+
+
+function decrypt(){  
+   
+    var str = document.getElementById("dec").value;
+    var enc = atob(str);
+    
+    
+         
+            document.getElementById("dec").value = enc;
+
+
 }   
