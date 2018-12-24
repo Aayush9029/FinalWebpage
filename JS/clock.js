f2e66bf7319e6f8364649eb7b183c670b28fc83e
Aayush9029 <aayushpokharel36@gmail.com>
2018-12-24 15:02:41 -0500

    clock added

---
+++ JS/clock.js
@@ -0,0 +1,55 @@
+//https://color.adobe.com/Copy-of-Copy-of-Billar-color-theme-11701107/edit/?copy=true&base=2&rule=Custom&selected=4&name=Copy%20of%20Copy%20of%20Copy%20of%20Billar&mode=rgb&rgbvalues=0.9490196078431372,0.23529411764705882,0.3137254901960784,1,0.7607843137254902,0.0196078431372549,0.9137254901960784,0.9450980392156862,0.8745098039215686,0.2901960784313726,0.8509803921568627,0.8509803921568627,0.00784313725490196,0.34509803921568627,0.6078431372549019&swatchOrder=0,1,2,3,4
+
+//above is my color pallet
+
+function setup() {
+ createCanvas(500, 500);
+ angleMode(DEGREES);
+}
+
+function draw(){
+  background(0);
+  translate(width/2,height/2);
+  rotate(-90);
+  let hr = hour();
+  let min = minute();
+  let sec = second();
+
+  noFill();
+  strokeWeight(6);
+  stroke(242, 60, 80);
+  let secondAngle = map(sec, 0,60, 0, 360);
+  arc(0, 0, width-200, width-200, 0, secondAngle);
+
+
+  stroke(74, 217, 217);
+  let minuteAngle = map(min, 0,60, 0, 360);
+  arc(0, 0,width-220, width-220, 0, minuteAngle)
+
+  stroke(2, 88, 155);
+  let hourAngle = map(hr % 12, 0, 12, 0, 360);
+  arc(0, 0, width-240, width-240, 0, hourAngle);
+
+  
+  push();
+  stroke(242, 60, 80);
+  rotate(secondAngle);
+  line(0,0,100,0)
+  pop();
+  
+  push();
+  stroke(74, 217, 217);
+  rotate(minuteAngle);
+  line(0,0,75,0)
+  pop();
+  
+  push();
+  stroke(2, 88, 155);
+  rotate(hourAngle);
+  line(0,0,50,0)
+  pop();
+
+  stroke(255);
+  point(0,0);
+
+}
