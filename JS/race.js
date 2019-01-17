6108f7854f5a43f0270dd9b9be697b7d1463f0e0
Aayush9029 <aayushpokharel36@gmail.com>
2019-01-17 11:22:24 -0500

    back

--- JS/race.js
+++ JS/race.js
@@ -9,28 +9,34 @@ let circler = 35;
 let yline = -5500;
 let r = 242;
 let points = 0;
+var img;
 
 function setup(){
 createCanvas(500,800);
+img = loadImage("../../Images/gamebg.png");
+
+
 }
 
 function draw(){
 
-
-    background(206,235,223); //sky ?
+    image(img, -45, 0);
+  // Displays the image at point (0, height/2) at half size
+//   image(img, 0, height/2, img.width/2, img.height/2);
+    // background(146,212,244); //sky ?
     noStroke();
-
-    textSize(16)
+    fill(51);
+    textSize(22)
     text(points,10, 10, 200, 200)
 
-    fill(255,246,229);//this is the line white in between
-    for (let i = 0 ; i < 25; i++){
-    rect(0, yline+i*250, 1200, 75);
-    }
+    // fill(255,246,229);//this is the line white in between
+    // for (let i = 0 ; i < 25; i++){
+    // rect(0, yline+i*250, 1200, 100);
+    // }
 
-    if (yline + 500 > height){
-        yline = -5500;
-    }
+    // if (yline + 500 > height){
+    //     yline = -5500;
+    // }
 
     yline += coinspeed;
     for (let i = 0; i <2; i++ ){
@@ -40,18 +46,18 @@ function draw(){
     yarrow= yarrow + coinspeed;
     // console.log(i);
     }
-    fill(4,139,199) //the rectangle (red?)
+    fill(101,183,243) //the rectangle (red?)
     rect(x,y,rectsize,rectsize);
 
-    fill(98,180,217) //the rectangle (red?)
+    fill(146,212,244) //the rectangle (red?)
     rect(x+2,y+2,18,10);
     x = x+ speed;
 
-    //bottom bar 
-    fill(156,212,252);
-    rect(0,random(740,741),700,70);
-    //water
-    fill(156,199,252);
+    //water top
+    fill(72,147,216);
+    rect(0,740,700,70);
+    //water bottom
+    fill(50,111,192);
     rect(0,750,700,70);
 
 
@@ -78,6 +84,7 @@ if(yarrow + circler >= y && y+rectsize >= yarrow){
 }else if ( yarrow > height){
     xarrow = random(width-circler);
     yarrow = 0;
+    speed == 4;
     // alert("Game Over dude you got, " + points + ' Points.');
     points = 0;
 }
@@ -92,7 +99,28 @@ if (x > width - rectsize){
     x = 0 ;
 }
 
-    
+
+if(points >= 0 && points < 5){
+    coinspeed = 5;
+    text('lvl:1',440, 10, 50, 50);
+
+}else if(points >= 5 && points < 15){
+    coinspeed = 5;
+    text('lvl:2',440, 10, 50, 50);
+
+}else if (points >= 15 && points < 25){
+    coinspeed = 6;
+text('lvl:3',440, 10, 50, 50);
+
+}else if (points >= 25 && points < 30){
+    coinspeed = 7;
+    text('lvl:4',440, 10, 50, 50);
+
+}else if (points >= 30){
+    coinspeed = 8;
+text('lvl:5',440, 10, 50, 50);
+
+}
 }
 
 
@@ -101,6 +129,8 @@ if (x > width - rectsize){
 function keyPressed(){
     if(keyCode == RIGHT_ARROW){
         speed = speed+5;
+       
+
     }
     if(keyCode == LEFT_ARROW){
         speed = speed-5;
