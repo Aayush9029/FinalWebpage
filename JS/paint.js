47dac4e0c12871bc3d86844c754dca794d86c4f3
Aayush9029 <aayushpokharel36@gmail.com>
2019-02-10 21:39:49 -0500

    overflow: hidden; lol

--- JS/paint.js
+++ JS/paint.js
@@ -1,46 +1,36 @@
 let time = 0;
-let rectsize = 25;
-
 let x = 200;
 let y = 200;
 let color = 255;
 let drawing = [];
 
 function setup(){
-    createCanvas(800,500);
+    createCanvas(800,550);
     background(255);
     redcolor = createSlider(0, 255, 100);
     greencolor = createSlider(0, 255, 100)   ;
     bluecolor = createSlider(0, 255, 100);
     strokevalue = createSlider(1, 20, 2, 1);
-    size = createSlider(10, 200, 25, 7);
-    strokevalue.position(90, 15);
     redcolor.position(90, 57);
     greencolor.position(90, 117);
     bluecolor.position(90, 172);
+    strokevalue.position(90, 15);
+
     
 
 }
 
-function keyPressed() {
-    if(keyCode == 67) {
-       shape = ellipse(mouseX, mouseY,size.value());
-    } else if (keyCode == 83) {
-        shape = rect(mouseX, mouseY, size.value(),size.value());
-    }
-    return 0;
-}
-
 
 function draw(){
-
     fill(redcolor.value(),greencolor.value(),bluecolor.value());  
     noStroke();
     rect(5, 5, 10, 10);
+
 }
 
 function mouseDragged(){
     strokeWeight(strokevalue.value());
     stroke(redcolor.value(),greencolor.value(),bluecolor.value());
     line(mouseX, mouseY, pmouseX, pmouseY);
+    console.log(mouseX + " y "+mouseY);
 }
