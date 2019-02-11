ae9a1b8168f3df7a17aaa0b32a18921d179edf0a
Aayush9029 <aayushpokharel36@gmail.com>
2019-02-10 21:11:10 -0500

    deleted uncessary stuff and fourier upgrade and added a wavelength featu

--- JS/fractal.js
+++ JS/fractal.js
@@ -5,6 +5,7 @@ let angle;
 function setup() {
     createCanvas(900,700);
     slider =  createSlider(0,TWO_PI, 5.9,0.01);
+    slider.position(width/2,height+45);
 }
 
 function draw(){
@@ -21,7 +22,7 @@ function branch(len){
 
     line(0, 0,0, -len);
     translate(0, -len);
-    if(len > 4){
+    if(len > 5){
     push();
     rotate(angle);
     branch(len * 0.67);
