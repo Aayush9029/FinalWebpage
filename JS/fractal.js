6b83e75ca339df9c72fc35ca70d0a9e9fbfa4dd9
Aayush <aayushpokharel36@gmail.com>
2019-03-13 15:37:25 -0400

    slider changed to click and new js added for fractal tree

--- JS/fractal.js
+++ JS/fractal.js
@@ -1,35 +1,69 @@
-
-let len;
-let angle;
+var tree = [];
+var leaves = [];
+let count = 0;
+let stop = 0;
 
 function setup() {
     createCanvas(900,700);
-    slider =  createSlider(0,TWO_PI, 5.9,0.01);
-    slider.position(width/2,height+45);
+    let a = createVector(width/2, height);
+    let b = createVector(width/2, height-200);
+    let root = new Branch(a, b);
+
+    tree[0] = root;
+
+
+}
+
+function mousePressed(){
+
+if ( stop < 1500){
+    for( var i = tree.length - 1 ; i >= 0; i--){
+        if(!tree[i].finished){
+        tree.push(tree[i].branchA());
+        tree.push(tree[i].branchB());
+        tree[i].finished = true;
+    
+        }
+        count++;
+        stop++; 
+        console.log(stop);
+
+        if(count == 69){
+            for (let i = 0; i <tree.length; i++){
+                if(!tree[i].finished){
+                    var leaf = tree[i].end.copy();
+                    leaves.push(leaf);
+                }
+
+            }
+        }
+        if(count == 420){
+            for (let i = 0; i <tree.length; i++){
+                if(!tree[i].finished){
+                    var leaf = tree[i].end.copy();
+                    leaves.push(leaf);
+                }
+
+            }
+        }
+    }
+}
 }
 
 function draw(){
     background(0);
-    strokeWeight(3);
-    stroke(255,random(2,90),146);
-    translate(width/2,height)
-    branch(200);
-    angle = slider.value();
+
+    for (let i = 0; i < tree.length ; i ++){
+        tree[i].show();
+        // tree[i].jitter();
+    }
+    for (let i = 0; i < leaves.length ; i ++){
+        noStroke();
+        fill(random(190,255),102,180, 180);
+        ellipse(leaves[i].x,leaves[i].y, 13,19);
+        leaves[i].y += random(-1/2,5);
+        leaves[i].x += random(1/2,1);
+
+    }
 }
 
-
-function branch(len){
-
-    line(0, 0,0, -len);
-    translate(0, -len);
-    if(len > 5){
-    push();
-    rotate(angle);
-    branch(len * 0.67);
-    pop();
-    push();
-    rotate(-angle);
-    branch(len * 0.67);
-    pop();
-}
-}
--- JS/particlebg.js
+++ JS/particlebg.js
@@ -1,3 +1,5 @@
+
+
 let particles = [];
 let r,g,b;
 let yspeed;
