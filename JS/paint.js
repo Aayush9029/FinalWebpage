de3e3c781afd35611bbd7460e2c550807859ef7b
Aayush <aayushpokharel36@gmail.com>
2019-03-19 09:45:04 -0400

    paint saving feature addeed

--- JS/paint.js
+++ JS/paint.js
@@ -1,36 +1,149 @@
-let time = 0;
-let x = 200;
-let y = 200;
-let color = 255;
-let drawing = [];
+var database;
+var drawing = [];
+var currentPath = [];
+var isDrawing = false;
 
 function setup(){
-    createCanvas(800,550);
-    background(255);
-    redcolor = createSlider(0, 255, 100);
+    canvas = createCanvas(400, 400); redcolor = createSlider(0, 255, 100);
     greencolor = createSlider(0, 255, 100)   ;
     bluecolor = createSlider(0, 255, 100);
-    strokevalue = createSlider(1, 20, 2, 1);
     redcolor.position(90, 57);
     greencolor.position(90, 117);
     bluecolor.position(90, 172);
-    strokevalue.position(90, 15);
 
+    canvas.mousePressed(startPath);
+    canvas.parent('canvascontainer');
+    canvas.mouseReleased(endPath);
     
-
-}
-
-
-function draw(){
-    fill(redcolor.value(),greencolor.value(),bluecolor.value());  
-    noStroke();
-    rect(5, 5, 10, 10);
-
-}
-
-function mouseDragged(){
-    strokeWeight(strokevalue.value());
-    stroke(redcolor.value(),greencolor.value(),bluecolor.value());
-    line(mouseX, mouseY, pmouseX, pmouseY);
-    console.log(mouseX + " y "+mouseY);
-}
+    var saveButton = select('#saveButton');
+    saveButton.mousePressed(saveDrawing);
+    
+    var clearButton = select('#clearButton');
+    clearButton.mousePressed(clearDrawing);
+    
+    
+    
+    
+    
+    var config = {
+            apiKey: "AIzaSyDVeRD0P145hETu39Ryh4HM8rvlTSj4Kos",
+            authDomain: "try2-70357.firebaseapp.com",
+            databaseURL: "https://try2-70357.firebaseio.com",
+            projectId: "try2-70357",
+            storageBucket: "",
+            messagingSenderId: "99106327684"
+        };
+    
+      firebase.initializeApp(config);
+      database = firebase.database();
+    
+        var ref = database.ref('drawings');
+        ref.on('value', gotData, errData);
+    } 
+    
+    function  startPath(){
+        isDrawing = true;
+        currentPath = [];
+        drawing.push(currentPath);
+        
+    }
+    
+    function  endPath(){
+        isDrawing = false;
+    
+    }
+    
+    function draw(){
+        background(51);
+    
+    
+        if(isDrawing){
+            var point = {
+                x: mouseX,
+                y: mouseY
+            }
+            currentPath.push(point);
+        }
+    
+        noFill();
+        stroke(redcolor.value(),greencolor.value(),bluecolor.value());  
+        for(var i = 0; i < drawing.length; i++){
+            var path = drawing[i];
+        beginShape();
+            for(var j = 0; j < path.length; j++){
+    
+            vertex(path[j].x, path[j].y);
+            endShape();
+            }
+        }
+    
+    }
+    
+    
+    
+    function saveDrawing(){
+        var ref = database.ref('drawings');
+        var data = {
+            name: 'Aayush',
+            drawing: drawing
+        }
+        var result = ref.push(data, dataSent);
+        console.log(result.key);
+    
+        function dataSent(status){
+            console.log(status)
+        }
+    }
+    
+    
+    
+    function gotData(data){
+    
+    //clear listing
+    
+    var elements = selectAll('.listing');
+    
+    for (var i = 0 ; i < elements.length; i++){
+        elements[i].remove();
+    }
+    
+    
+        var drawings = data.val();
+        var keys = Object.keys(drawings);
+        for(var i = 0; i < keys.length ; i++){
+            var key = keys[i];
+            // console.log(key)
+            var li = createElement('li', key);
+            li.class('listing');
+            var ahref = createA('#', key);
+            ahref.mousePressed(showDrawing);
+            ahref.parent(li);
+            li.parent('drawinglist');
+        }
+    }
+    
+    function errData(err){
+        console.log(err);
+    }
+    
+    function showDrawing(){
+        var key = this.html();
+        var ref = database.ref('drawings/' + key);
+        ref.once('value', oneDrawing, errData);
+        
+        function oneDrawing(data){
+            var dbdrawing = data.val();
+            drawing = dbdrawing.drawing;
+            // console.log(drawing);
+        }
+    
+    
+    
+    }
+    
+    
+    
+    
+    function clearDrawing(){
+        drawing = [];
+    }
