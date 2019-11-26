eabe67cd0ad8648cf8a84cb943c0c7377bc87864
Aayush <aayushpokharel36@gmail.com>
2019-11-25 21:13:52 -0500

    updated weather
    new morden design bye crappy design

--- JS/weather.js
+++ JS/weather.js
@@ -1,67 +1,74 @@
 //for date & time manupulation
-currentDate = new Date();
-date2 = currentDate.getMonth()+1;
-date = currentDate.getDate();
-dayfinal = ("Month " + date2 +", Day " + date);
-
+let days = "Sat Sun Mon Tue Wed Thu Fri".split(" ");
+const date = new Date();
+const day = date.getDate();
+const week = date.getDay();
+const month = date.toLocaleString("default", { month: "short" }).split(".")[0];
+console.log(days[week], day, month);
+const finalDate = days[week] + ", " + day + " " + month;
 
 //jquery manupulation
-$(document).ready(function () {
-    
+$(document).ready(function() {
   // Get Location (asks for location access !!
   navigator.geolocation.getCurrentPosition(success, error);
 
   function success(pos) {
-      var lat = pos.coords.latitude;
-      var long = pos.coords.longitude;
-      weather(lat, long);
+    console.log(pos);
+    var lat = pos.coords.latitude;
+    var long = pos.coords.longitude;
+    weather(lat, long);
   }
 
   function error() {
-      console.log('There was an error');
+    console.log("There was an error");
   }
 
   // Call Weather
   function weather(lat, long) {
-      var URL = `https://api.openweathermap.org/data/2.5/weather?APPID=bc491408821cc43317006173fd1c5bef&lat=${lat}&lon=${long}&units=metric`;
+    var URL = `https://api.openweathermap.org/data/2.5/weather?APPID=bc491408821cc43317006173fd1c5bef&lat=${lat}&lon=${long}&units=metric`;
 
-      $.getJSON(URL, function(data) {
-          updateDOM(data);
-          console.log(data);
-      });
+    $.getJSON(URL, function(data) {
+      updateDOM(data);
+    });
   }
 
   // Update Dom
   function updateDOM(data) {
-      var city = (data.name);
-      var temp = Math.round(data.main.temp);
-      var desc = data.weather[0].description;
-      var max = data.main.temp_max;
-      var min = data.main.temp_min;
-      var wind = data.wind.speed;
-      var humid = data.main.humidity;
-      var greeting;
-      
-        if (temp > 15) {
-        greeting = "No Jacket Necessary";
+    var city = data.name;
+    var temp = Math.round(data.main.temp);
+    var desc = data.weather[0].description;
+    var max = Math.round(data.main.temp_max);
+    var min = Math.round(data.main.temp_min);
+    // Math.fl
+    var pressure = data.main.pressure;
 
-         }else if (temp < 5){
-          greeting ='Heavy Jacket';
-          }else {
-         greeting ='light jacket';
-        }
+    var wind = data.wind.speed;
+    var humid = data.main.humidity;
 
-      $('#city').html(city.toUpperCase());
-      $('#temp').html(temp);
-      $('#desc').html(desc.toUpperCase());
-      $('#wear').html(greeting);
-      $('#minmax').html('Today will be ' + min +' to '+ max);
-     
-      $('#wind').html('Wind: '+ wind + " km/h");
-      $('#humid').html('Humidity: ' + humid);
+    if (temp > 15) {
+      greeting = "No Jacket Necessary";
+    } else if (temp < 5) {
+      greeting = "Heavy Jacket";
+    } else {
+      greeting = "light jacket";
+    }
 
-      $('#copyright').html('  (c) aayushpokharel, (p.s) for people who wanna know more console log :)');
+    console.log(data);
+    $("#city").html(city.toUpperCase());
+    $("#temp").html(temp + "°");
+    $("#desc").html(desc.toUpperCase());
+    // $("#wear").html(greeting);
+    $("#min").html(min + "°");
 
+    $("#max").html(min + "°");
+    $("#pressure").html(pressure + " km/h");
+
+    $("#wind_speed").html(wind + " km/h");
+    $("#humid").html(humid + "%");
+    $("#time").html(finalDate);
+
+    // $("#copyright").html(
+    //   "  (c) aayushpokharel, (p.s) for people who wanna know more console log :)"
+    // );
   }
 });
-
